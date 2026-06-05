1f0858001b6e132db98781224582292ed0a24056
Alan Tavares <alan1tavares@gmail.com>
2026-06-04 23:46:11 -0300

    feat: campos CNPJ e Série estilizados com ícones e máscara de CNPJ
    Adiciona ícone de prédio e máscara automática (00.000.000/0000-00) no
    campo CNPJ via TextChanged, e ícone de documento com label
    'Documento (Série)'. Dígitos puros são extraídos antes de gerar as chaves.

--- GeradorChaveNFe/MainPage.xaml.cs
+++ GeradorChaveNFe/MainPage.xaml.cs
@@ -10,6 +10,24 @@ public partial class MainPage : ContentPage
         uFPicker.ItemsSource = UfRepository.GetItems();
     }
 
+    private void OnCnpjTextChanged(object sender, TextChangedEventArgs e)
+    {
+        var entry = (Entry)sender;
+        var digits = new string(e.NewTextValue.Where(char.IsDigit).Take(14).ToArray());
+
+        var formatted = digits.Length switch
+        {
+            <= 2  => digits,
+            <= 5  => $"{digits[..2]}.{digits[2..]}",
+            <= 8  => $"{digits[..2]}.{digits[2..5]}.{digits[5..]}",
+            <= 12 => $"{digits[..2]}.{digits[2..5]}.{digits[5..8]}/{digits[8..]}",
+            _     => $"{digits[..2]}.{digits[2..5]}.{digits[5..8]}/{digits[8..12]}-{digits[12..]}"
+        };
+
+        if (entry.Text != formatted)
+            entry.Text = formatted;
+    }
+
     private void OnGerarChavesClicked(object sender, EventArgs e)
     {
         if (uFPicker.SelectedItem is not UFModel uf)
@@ -20,7 +38,7 @@ public partial class MainPage : ContentPage
             CodigoUf = uf.Codigo,
             Mes = int.Parse(mesEntry.Text),
             Ano = int.Parse(anoEntry.Text),
-            CNPJ = cnpjEntry.Text,
+            CNPJ = new string(cnpjEntry.Text.Where(char.IsDigit).ToArray()),
             Serie = int.Parse(serieEntry.Text),
             NumeroNotaInicial = int.Parse(numeroNotaInicialEntry.Text),
             NumeroNotaFinal = int.Parse(numeroNotaFinalEntry.Text),
